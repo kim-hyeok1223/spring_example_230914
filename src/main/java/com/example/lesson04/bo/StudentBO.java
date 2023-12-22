bc3e04d52da9605d0a00ba8d9b6e5977464b1485
test <test@test.com>
2023-12-22 18:46:54 +0900

    1222 수업

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -14,4 +14,8 @@ public class StudentBO {
 	public void addStudent(Student student) {
 		studentMapper.insertStudent(student);
 	}
+	
+	public Student getStudentById(int id) {
+		return studentMapper.selectStudentById(id);
+	}
 }
